490948ed26972beaa74a66df1de6ae5894f1d609
Pavel Glukhov <pvglukhov@gmail.com>
2023-05-27 02:02:35 +0300

    [master] some addition

---
+++ src/test/java/guru/springframework/GreatingTest.java
@@ -0,0 +1,41 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.*;
+
+import javax.crypto.KeyAgreement;
+
+
+class GreatingTest {
+    private static Greating greating;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("In before");
+        greating = new Greating();
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        System.out.println("Before all");
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greating.helloWorld());
+    }
+
+    @Test
+    void testHelloWorld() {
+        System.out.println(greating.helloWorld("John"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In after");
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        System.out.println("After all");
+    }
+}
